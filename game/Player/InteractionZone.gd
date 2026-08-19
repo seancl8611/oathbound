cd7a0e314e7b663179ff9abaa3c6b0e55c851d6b
Sean Clarke <seanc8611@gmail.com>
2026-08-18 20:53:29 -0400

    Delete game/Player directory

--- game/Player/InteractionZone.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@export var damage = 10
-@onready var collision_shape = $CollisionShape2D
-
-func _on_area_entered(area):
-	if area.is_in_group("player_hurtbox"):
-		if area.has_signal("hurt"):
-			area.emit_signal("hurt", damage, "normal", self)
