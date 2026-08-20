322c7667fc3d89adae71ffb2e5cea340a9746e88
Sean Clarke <seanc8611@gmail.com>
2026-08-19 22:17:09 -0400

    Point Archer rules at canonical controller

--- game/oathbound/Regions/Hushiro/Enemies/Standard/CorruptedArcherRules.gd
+++ game/oathbound/Regions/Hushiro/Enemies/Standard/CorruptedArcherRules.gd
@@ -1,4 +1,4 @@
-extends "res://Enemy/Area 1/Encounter/corrupted_archer.gd"
+extends "res://Regions/Hushiro/Enemies/Standard/CorruptedArcherController.gd"
 
 ## Current Hushiro rules layer over the imported Archer movement/aiming controller.
 ## The inherited firing-line spacing and retreat behavior already match the approved
@@ -18,4 +18,4 @@ func _ready() -> void:
 	block_by_default = false
 	block_chance_on_hit = 0.20
 
-	print("[HushiroArcher] 75 Health / 65 Posture firing-line contract active")
+	print("[CorruptedArcherRules] 75 Health / 65 Posture firing-line contract active")
